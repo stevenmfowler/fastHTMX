3a44d24ff43efaf6919a34b50165d717ef91432a
stevenmfowler <stevenfowler@live.com>
2024-04-30 21:38:25 -0400

    fix z-index issues

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
-from pathlib import Path
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-BASE_PATH = Path(__file__).resolve().parent
 templates = Jinja2Templates(directory='templates')
 
 @app.get('/', response_class=HTMLResponse)
